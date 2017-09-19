1ab317845af44c8ccfdda0011c76cdfa64777918
谢梦扬 <shel@xiemengyangdeMacBook-Pro.local>
2017-09-19 20:54:54 +0800

    3

--- src/js/language.js
+++ src/js/language.js
@@ -1,25 +1,19 @@
-  
-  
-  var value = sessionStorage.getItem("language");
 
-    document.onreadystatechange = function () {
-        if (document.readyState === 'complete') {
-             $('#loading').fadeOut()
-            if(value==="1"){
-                Microsoft.Translator.Widget.Translate('zh-CHS', 'en', onProgress, onError, onComplete, onRestoreOriginal, 2000);
-            }
-        }
-    }
-
-function translate(){
-
-      var script=document.createElement("script");  
+    var script=document.createElement("script");  
     script.type="text/javascript";  
     script.src="http://www.microsoftTranslator.com/ajax/v3/WidgetV3.ashx?siteData=ueOIGRSKkd965FeEGM5JtQ**";  
     document.getElementsByTagName('head')[0].appendChild(script);  
 
 
-  
+    var value = sessionStorage.getItem("language");
+    document.onreadystatechange = function () {
+        if (document.readyState == 'complete') {
+            $('#loading').fadeOut()
+            if(value==="1"){
+                Microsoft.Translator.Widget.Translate('zh-CHS', 'en', onProgress, onError, onComplete, onRestoreOriginal, 2000);
+            }
+        }
+    }
     function onProgress(value) {
     }
     function onError(error) {
@@ -29,10 +23,12 @@ function translate(){
     }
     function onRestoreOriginal() { 
     }
+
+
+function translate(){
     var value = sessionStorage.getItem("language");
-    if(value==="1"){ 
-        sessionStorage.setItem("language", "0");
-       
+    if(value==="1"){
+        sessionStorage.setItem("language", "0"); 
     }else{
         sessionStorage.setItem("language", "1");
     }
